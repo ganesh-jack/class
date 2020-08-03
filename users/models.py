642fe3f8b02f21379f4b7940e11a4aac46dbfda5
Ganesh M <60595232+ganesh-jack@users.noreply.github.com>
2020-08-03 04:35:29 -0700

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    skillsets = models.CharField(max_length=100, default="No skill sets assigned")
+    tools_known = models.CharField(max_length=150, default="No tools are assigned")
+    computer_languages_known = models.CharField(max_length=100, default="Languages are not assigned")
+    department = models.CharField(max_length=30, default='EEE' )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        
+        try:
+            img = Image.open(self.image.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
+        except:
+            pass
